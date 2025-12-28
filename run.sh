2c3985b8d7320ea407375f48584f022804e93213
fishyi <yixiaoyu@stu.pku.edu.cn>
2025-12-28 12:25:55 +0000

    fix some path

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 python demo.py \
     --model_path ./checkpoint \
-    --lyrics ./heartmula_gen/samples/lyrics.txt \
-    --tags ./heartmula_gen/samples/tags.txt \
-    --save_path ./heartmula_gen/samples/result.wav \
+    --lyrics ./samples/lyrics.txt \
+    --tags ./samples/tags.txt \
+    --save_path ./samples/result.wav \
     --topk 50 \
     --temperature 1.0 \
     --cfg_scale 1.5 \
